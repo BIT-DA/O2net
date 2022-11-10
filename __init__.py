2d30123ce8a194a26ffcaae1a037ae0257e4d5fa
kxgong <43208919+kxgong@users.noreply.github.com>
2022-11-10 14:06:03 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,18 @@
+# ------------------------------------------------------------------------
+# Deformable DETR
+# Copyright (c) 2020 SenseTime. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
+# Modified from DETR (https://github.com/facebookresearch/detr)
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# ------------------------------------------------------------------------
+
+from .deformable_detr import build
+from .DA_deformable_detr import DA_build 
+
+def build_model(args):
+    return build(args)
+
+def DA_build_model(args):
+    return DA_build(args)
+
